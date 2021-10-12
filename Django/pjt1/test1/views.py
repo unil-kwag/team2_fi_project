7081024493eb9ed13be324abad87fd0451ec937f
unil-kwag <85001573+unil-kwag@users.noreply.github.com>
2021-10-12 09:58:59 +0900

    네이버 API 뉴스 1
    변경점:
    검색 후 화면 전환(?search=검색어)
    헤드라인, 신문 요약

--- Django/pjt1/test1/views.py
+++ Django/pjt1/test1/views.py
@@ -1,10 +1,13 @@
+from .forms import Form
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db import connection
 from .models import *
 from django.utils import timezone
 
 import os
- 
+
+import requests
+import re
 import pandas as pd
 import json
 
@@ -35,6 +38,7 @@ from django_plotly_dash import DjangoDash
 #     context = {'search_result1': data}
 #     return render(request, 'test1/index.html', context)
 
+
 def index(request):
     gu = request.GET.get('select_gu')
     kind = request.GET.get('select_kind')
@@ -47,10 +51,10 @@ def index(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'gu':i[0]}
+        row = {'gu': i[0]}
         data.append(row)
     context = {}
-    context['search_result1'] = data;
+    context['search_result1'] = data
     if gu != None and gu != '':
         cursor = connection.cursor()
         strSql = f'SELECT DISTINCT supply_type FROM seoul WHERE address_gu = "{gu}"'
@@ -64,7 +68,7 @@ def index(request):
             data.append(row)
         context['select_gu'] = gu
         context['housing_kind'] = data
-        
+
     if kind != None and kind != '':
         cursor = connection.cursor()
         strSql = f'SELECT DISTINCT land_name FROM seoul WHERE address_gu = "{gu}" and supply_type = "{kind}"'
@@ -82,7 +86,6 @@ def index(request):
     return render(request, 'test1/index.html', context)
 
 
-
 # def select_gu(request):
 #     gu = request.GET.get('select_gu')
 #     data = []
@@ -96,23 +99,26 @@ def index(request):
 #         context = {'housing_kind': data}
 #     return render(request, 'test1/index.html', context)
 
+# ====================================================================================================================
+# ====================================================================================================================
+# 도준 ===============================================================================================================
 
-##====================================================================================================================
-##====================================================================================================================
-## 도준 ===============================================================================================================
-from .forms import Form
 
 def selection(request):
     test = request.GET.get('name')
-    return render(request, 'test1/selection.html',{'test':test})
+    return render(request, 'test1/selection.html', {'test': test})
+
 
 def seoul(request):
-    return render(request, 'test1/map/seoul.html',{})
+    return render(request, 'test1/map/seoul.html', {})
+
+
 def map(request):
-    return render(request, 'test1/map/songpa_result.html',{})
-##====================================================================================================================
-##====================================================================================================================
-##=================================================================================================================도준
+    return render(request, 'test1/map/songpa_result.html', {})
+# ====================================================================================================================
+# ====================================================================================================================
+# =================================================================================================================도준
+
 
 def search(request):
     gu = request.GET.get('select_gu')
@@ -122,63 +128,100 @@ def search(request):
     context['select_gu'] = gu
     context['select_kind'] = kind
     context['select_name'] = name
-    return render(request, 'test1/search.html',context)
+    return render(request, 'test1/search.html', context)
+
 
 def news(request):
-    return render(request, 'test1/news.html',{})
+    return render(request, 'test1/news.html', {})
+
 
 def board_main(request):
-    return render(request, 'test1/board_main.html',{})
+    return render(request, 'test1/board_main.html', {})
+
 
 def board_write(request):
-    return render(request, 'test1/board_write.html',{})    
+    return render(request, 'test1/board_write.html', {})
+
 
 def board_view(request):
-    return render(request, 'test1/board_view.html',{})    
+    return render(request, 'test1/board_view.html', {})
+
 
 def board_edit(request):
-    return render(request, 'test1/board_edit.html',{})    
+    return render(request, 'test1/board_edit.html', {})
+
 
 def home(request):
     blogs = Blog.objects.all().order_by('-id')
-    return render(request, 'test1/home.html', {'blogs':blogs})
-    # render라는 함수를 통해 페이지를 띄워줄 건데, home.html 파일을 띄워줄 것이고 
+    return render(request, 'test1/home.html', {'blogs': blogs})
+    # render라는 함수를 통해 페이지를 띄워줄 건데, home.html 파일을 띄워줄 것이고
     # 이 때, blogs 객체도 함께 넘겨주도록 하겠다.
 
-def detail(request, blog_id): 
-    blog_detail = get_object_or_404(Blog, pk= blog_id) # 특정 객체 가져오기(없으면 404 에러)
-    return render(request, 'test1/detail.html', {'blog':blog_detail})
-    # render라는 함수를 통해 페이지를 띄워줄 건데, home.html 파일을 띄워줄 것이고 
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id)  # 특정 객체 가져오기(없으면 404 에러)
+    return render(request, 'test1/detail.html', {'blog': blog_detail})
+    # render라는 함수를 통해 페이지를 띄워줄 건데, home.html 파일을 띄워줄 것이고
     # 이 때, blog 객체도 함께 넘겨주도록 하겠다.
+
 
 def new(request):
     return render(request, 'test1/new.html')
 
+
 def create(request):
-    blog = Blog() # 객체 틀 하나 가져오기
+    blog = Blog()  # 객체 틀 하나 가져오기
     blog.title = request.GET['title']
     blog.writer = request.GET['writer']  # 내용 채우기
-    blog.body = request.GET['body'] # 내용 채우기
-    blog.pub_date = timezone.datetime.now() # 내용 채우기
-    blog.save() # 객체 저장하기
+    blog.body = request.GET['body']  # 내용 채우기
+    blog.pub_date = timezone.datetime.now()  # 내용 채우기
+    blog.save()  # 객체 저장하기
     # 새로운 글 url 주소로 이동
     return redirect('/blog/' + str(blog.id))
 
-def edit(request,blog_id):
-    blog= get_object_or_404(Blog, pk= blog_id) # 특정 객체 가져오기(없으면 404 에러)
-    return render(request, 'test1/edit.html', {'blog':blog})
+
+def edit(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)  # 특정 객체 가져오기(없으면 404 에러)
+    return render(request, 'test1/edit.html', {'blog': blog})
+
 
 def delete(request, blog_id):
-    blog= get_object_or_404(Blog, pk= blog_id) # 특정 객체 가져오기(없으면 404 에러)
+    blog = get_object_or_404(Blog, pk=blog_id)  # 특정 객체 가져오기(없으면 404 에러)
     blog.delete()
-    return redirect('home') # home 이름의 url 로
+    return redirect('home')  # home 이름의 url 로
 
 # U - update(기존 글 객체 가져와서 수정하기)
-def update(request,blog_id):
-    blog= get_object_or_404(Blog, pk= blog_id) # 특정 객체 가져오기(없으면 404 에러)
+
+
+def update(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)  # 특정 객체 가져오기(없으면 404 에러)
     blog.title = request.GET['title']
     # blog.writer = request.GET['writer']
-    blog.body = request.GET['body'] # 내용 채우기
-    blog.pub_date = timezone.datetime.now() # 내용 채우기
-    blog.save() # 저장하기
+    blog.body = request.GET['body']  # 내용 채우기
+    blog.pub_date = timezone.datetime.now()  # 내용 채우기
+    blog.save()  # 저장하기
     return redirect('/blog/' + str(blog.id))
+
+
+def news(request):
+    search = request.GET.get('search')
+    page = request.GET.get('page')
+
+    client_id = "6HlcFx6Fi1uXNPCW7pmG"
+    client_secret = "6Hx6DIiFP_"
+
+    encode_type = 'json'  # 출력 방식 json 또는 xml
+    max_display = 5  # 출력 뉴스 수
+    sort = 'date'  # 결과값의 정렬기준 시간순 date, 관련도 순 sim
+    start = 1  # 출력 위치
+
+    url = f"https://openapi.naver.com/v1/search/news.{encode_type}?query={search}&display={str(int(max_display))}&start={str(int(start))}&sort={sort}"
+
+    # 헤더에 아이디와 키 정보 넣기
+    headers = {'X-Naver-Client-Id': client_id,
+               'X-Naver-Client-Secret': client_secret
+               }
+
+    # HTTP요청 보내기
+    r = requests.get(url, headers=headers)
+    return render(request, 'test1/news.html', {'result': r.json()})
