87982a28a9809bc8e4f1951c8feb4e751840c312
unil-kwag <85001573+unil-kwag@users.noreply.github.com>
2021-10-13 21:14:42 +0900

    main page API 뉴스 호출 1
    main page news API 호출

--- Django/pjt1/pjt1/urls.py
+++ Django/pjt1/pjt1/urls.py
@@ -14,35 +14,37 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 from test1.views import *
 import test1.views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index,name='main'),
-    
-
-    path('selection/',selection,name='Location_Selection'),
-    path('songpa/',songpa,name='map'),
-    path('seoul/',seoul,name='seoul'),
-    path('test/',test,name='test'),
+    path('', news1, name='main'),
+    path('index/', news1, name='main'),
 
 
-    path('search/',search,name='Search'),
-    path('news/',news,name='News'),
-    path('board_main/',board_main,name='Board_main'),
-    path('board_write/',board_write,name='Board_write'),
-    path('board_view/',board_view,name='Board_view'),
-    path('board_edit/',board_edit,name='Board_edit'),
-    
+
+    path('selection/', selection, name='Location_Selection'),
+    path('songpa/', songpa, name='map'),
+    path('seoul/', seoul, name='seoul'),
+    path('test/', test, name='test'),
+
+
+    path('search/', search, name='Search'),
+    path('news/', news, name='News'),
+    path('board_main/', board_main, name='Board_main'),
+    path('board_write/', board_write, name='Board_write'),
+    path('board_view/', board_view, name='Board_view'),
+    path('board_edit/', board_edit, name='Board_edit'),
+
     path('django_dash/', include('django_plotly_dash.urls')),
     path('blog/', home, name='home'),
     path('blog/<int:blog_id>', detail, name="detail"),
     path('blog/new', new, name="new"),
     path('blog/create', create, name='create'),
-    path('blog/edit/<int:blog_id>',edit, name="edit"),
+    path('blog/edit/<int:blog_id>', edit, name="edit"),
     path('blog/update/<int:blog_id>', update, name="update"),
     path('blog/delete/<int:blog_id>', delete, name="delete"),
 ]
--- Django/pjt1/test1/views.py
+++ Django/pjt1/test1/views.py
@@ -32,7 +32,6 @@ from haversine import haversine
 # Create your views here.
 
 
-
 # ====================================================================================================================
 # 도영 ===============================================================================================================
 def index(request):
@@ -81,6 +80,7 @@ def index(request):
         context['select_name'] = name
     return render(request, 'test1/index.html', context)
 
+
 def search(request):
     gu = request.GET.get('select_gu')
     print(gu)
@@ -99,7 +99,7 @@ def search(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'apt_name' : i[1], 'apt_lon' : i[2], 'apt_lat' : i[3]}
+        row = {'apt_name': i[1], 'apt_lon': i[2], 'apt_lat': i[3]}
         data.append(row)
     context['select_apt'] = data
     apt_df = pd.DataFrame(context['select_apt'])
@@ -112,7 +112,7 @@ def search(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'bus_name' : i[1], 'bus_lon' : i[2], 'bus_lat' : i[3]}
+        row = {'bus_name': i[1], 'bus_lon': i[2], 'bus_lat': i[3]}
         data.append(row)
     context['all_bus'] = data
     bus_df = pd.DataFrame(context['all_bus'])
@@ -125,7 +125,7 @@ def search(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'care_name' : i[1], 'care_lon' : i[2], 'care_lat' : i[3]}
+        row = {'care_name': i[1], 'care_lon': i[2], 'care_lat': i[3]}
         data.append(row)
     context['all_care'] = data
     care_df = pd.DataFrame(context['all_care'])
@@ -138,7 +138,8 @@ def search(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'convenience_name' : i[0], 'convenience_lon' : i[1], 'convenience_lat' : i[2]}
+        row = {'convenience_name': i[0],
+               'convenience_lon': i[1], 'convenience_lat': i[2]}
         data.append(row)
     context['all_convenience'] = data
     convenience_df = pd.DataFrame(context['all_convenience'])
@@ -151,7 +152,7 @@ def search(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'depart_name' : i[0], 'depart_lon' : i[1], 'depart_lat' : i[2]}
+        row = {'depart_name': i[0], 'depart_lon': i[1], 'depart_lat': i[2]}
         data.append(row)
     context['all_depart'] = data
     depart_df = pd.DataFrame(context['all_depart'])
@@ -164,7 +165,7 @@ def search(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'fire_name' : i[1], 'fire_lon' : i[2], 'fire_lat' : i[3]}
+        row = {'fire_name': i[1], 'fire_lon': i[2], 'fire_lat': i[3]}
         data.append(row)
     context['all_fire'] = data
     fire_df = pd.DataFrame(context['all_fire'])
@@ -177,7 +178,8 @@ def search(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'hospital_name' : i[1], 'hospital_lon' : i[2], 'hospital_lat' : i[3]}
+        row = {'hospital_name': i[1],
+               'hospital_lon': i[2], 'hospital_lat': i[3]}
         data.append(row)
     context['all_hospital'] = data
     hospital_df = pd.DataFrame(context['all_hospital'])
@@ -190,7 +192,7 @@ def search(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'kinder_name' : i[1], 'kinder_lon' : i[2], 'kinder_lat' : i[3]}
+        row = {'kinder_name': i[1], 'kinder_lon': i[2], 'kinder_lat': i[3]}
         data.append(row)
     context['all_kinder'] = data
     kinder_df = pd.DataFrame(context['all_kinder'])
@@ -203,10 +205,10 @@ def search(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'parking_name' : i[0], 'parking_lon' : i[1], 'parking_lat' : i[2]}
+        row = {'parking_name': i[0], 'parking_lon': i[1], 'parking_lat': i[2]}
         data.append(row)
     context['all_parking'] = data
-    parking_df = pd.DataFrame(context['all_parking'])    
+    parking_df = pd.DataFrame(context['all_parking'])
 # 약국 데이터 불러오기---------------------------------------
     cursor = connection.cursor()
     strSql = f'SELECT  DISTINCT  id, name, longitude, latitude FROM pharmacy'
@@ -216,10 +218,11 @@ def search(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'pharmacy_name' : i[1], 'pharmacy_lon' : i[2], 'pharmacy_lat' : i[3]}
+        row = {'pharmacy_name': i[1],
+               'pharmacy_lon': i[2], 'pharmacy_lat': i[3]}
         data.append(row)
     context['all_pharmacy'] = data
-    pharmacy_df = pd.DataFrame(context['all_pharmacy'])    
+    pharmacy_df = pd.DataFrame(context['all_pharmacy'])
 # 경찰서 데이터 불러오기---------------------------------------
     cursor = connection.cursor()
     strSql = f'SELECT  DISTINCT  id, police_station, longitude, latitude FROM police'
@@ -229,10 +232,10 @@ def search(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'police_name' : i[1], 'police_lon' : i[2], 'police_lat' : i[3]}
+        row = {'police_name': i[1], 'police_lon': i[2], 'police_lat': i[3]}
         data.append(row)
     context['all_police'] = data
-    police_df = pd.DataFrame(context['all_police'])    
+    police_df = pd.DataFrame(context['all_police'])
 # 우체국 데이터 불러오기---------------------------------------
     cursor = connection.cursor()
     strSql = f'SELECT  DISTINCT  id, longitude, latitude FROM post'
@@ -242,10 +245,10 @@ def search(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'post_name' : i[0], 'post_lon' : i[1], 'post_lat' : i[2]}
+        row = {'post_name': i[0], 'post_lon': i[1], 'post_lat': i[2]}
         data.append(row)
     context['all_post'] = data
-    post_df = pd.DataFrame(context['all_post'])    
+    post_df = pd.DataFrame(context['all_post'])
 # 학교 데이터 불러오기---------------------------------------
     cursor = connection.cursor()
     strSql = f'SELECT  DISTINCT id, name, longitude, latitude FROM school'
@@ -255,10 +258,10 @@ def search(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'school_name' : i[1], 'school_lon' : i[2], 'school_lat' : i[3]}
+        row = {'school_name': i[1], 'school_lon': i[2], 'school_lat': i[3]}
         data.append(row)
     context['all_school'] = data
-    school_df = pd.DataFrame(context['all_school'])    
+    school_df = pd.DataFrame(context['all_school'])
 # 상점 데이터 불러오기---------------------------------------
     cursor = connection.cursor()
     strSql = f'SELECT  DISTINCT id, small_category, longitude, latitude FROM store'
@@ -268,10 +271,10 @@ def search(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'store_name' : i[1], 'store_lon' : i[2], 'store_lat' : i[3]}
+        row = {'store_name': i[1], 'store_lon': i[2], 'store_lat': i[3]}
         data.append(row)
     context['all_store'] = data
-    store_df = pd.DataFrame(context['all_store'])        
+    store_df = pd.DataFrame(context['all_store'])
 # 지하철 데이터 불러오기---------------------------------------
     cursor = connection.cursor()
     strSql = f'SELECT  DISTINCT id, station_num, longitude, latitude FROM subway'
@@ -281,35 +284,36 @@ def search(request):
     connection.close()
     data = []
     for i in search_result:
-        row = {'subway_name' : i[1], 'subway_lon' : i[2], 'subway_lat' : i[3]}
+        row = {'subway_name': i[1], 'subway_lon': i[2], 'subway_lat': i[3]}
         data.append(row)
     context['all_subway'] = data
-    subway_df = pd.DataFrame(context['all_subway'])        
+    subway_df = pd.DataFrame(context['all_subway'])
 # 아파트-버스 1km 필터---------------------------------------
     make_distance = pd.DataFrame()
     for i in range(len(bus_df)):
-        distance = haversine( tuple(apt_df.iloc[0, 1:]), tuple(bus_df.iloc[:, 1:].values[i]), unit = 'km')
+        distance = haversine(tuple(apt_df.iloc[0, 1:]), tuple(
+            bus_df.iloc[:, 1:].values[i]), unit='km')
         if distance <= 1:
             one_k = {
-                'apt_name' : apt_df.iloc[0, 0],
-                'bus_name' : bus_df.iloc[:, 0].values[i],
-                'bus_lon' : bus_df.iloc[:, 1].values[i],
-                'bus_lat' : bus_df.iloc[:, 2].values[i]
+                'apt_name': apt_df.iloc[0, 0],
+                'bus_name': bus_df.iloc[:, 0].values[i],
+                'bus_lon': bus_df.iloc[:, 1].values[i],
+                'bus_lat': bus_df.iloc[:, 2].values[i]
             }
             make_distance = make_distance.append(one_k, ignore_index=True)
     lat = apt_df.iloc[0, 2]
     lon = apt_df.iloc[0, 1]
     name = apt_df.iloc[0, 0]
-    map = folium.Map(location = [lat, lon], zoom_start=14)
-    folium.Marker( [lat, lon],
-                  icon = (folium.Icon(icon='home', prefix='fa', color='green')),
-                  tooltip = name
-    ).add_to(map)
-    folium.Circle( [lat, lon],
-                    radius = 1000, popup = '편의시설 범위'
-    ).add_to(map)
-    maps=map._repr_html_()
-    return render(request, 'test1/search.html',{'map' : maps})
+    map = folium.Map(location=[lat, lon], zoom_start=14)
+    folium.Marker([lat, lon],
+                  icon=(folium.Icon(icon='home', prefix='fa', color='green')),
+                  tooltip=name
+                  ).add_to(map)
+    folium.Circle([lat, lon],
+                  radius=1000, popup='편의시설 범위'
+                  ).add_to(map)
+    maps = map._repr_html_()
+    return render(request, 'test1/search.html', {'map': maps})
 
 # ====================================================================================================================
 # ====================================================================================================================
@@ -498,3 +502,67 @@ def news(request):
                    'pubDate': i['pubDate']}
         result.append(tmp_dic)
     return render(request, 'test1/news.html', {'result': result})
+
+
+def news1(request):
+    search = '서울 부동산'
+    page = request.GET.get('page')
+
+    client_id = "6HlcFx6Fi1uXNPCW7pmG"
+    client_secret = "6Hx6DIiFP_"
+
+    encode_type = 'json'  # 출력 방식 json 또는 xml
+    max_display = 2  # 출력 뉴스 수
+    sort = 'date'  # 결과값의 정렬기준 시간순 date, 관련도 순 sim
+    start = 1  # 출력 위치
+
+    url = f"https://openapi.naver.com/v1/search/news.{encode_type}?query={search}&display={str(int(max_display))}&start={str(int(start))}&sort={sort}"
+
+    # 헤더에 아이디와 키 정보 넣기
+    headers = {'X-Naver-Client-Id': client_id,
+               'X-Naver-Client-Secret': client_secret
+               }
+
+    # HTTP요청 보내기
+    r = requests.get(url, headers=headers)
+    result = []
+    for i in r.json()['items']:
+        tmp_dic = {'title': clean_html(i['title']),
+                   'originallink': i['originallink'],
+                   'link': i['link'],
+                   'description': clean_html(i['description']),
+                   'pubDate': i['pubDate']}
+        result.append(tmp_dic)
+    return render(request, 'test1/index.html', {'result': result})
+
+
+def news(request):
+    search = '서울 부동산'
+    page = request.GET.get('page')
+
+    client_id = "6HlcFx6Fi1uXNPCW7pmG"
+    client_secret = "6Hx6DIiFP_"
+
+    encode_type = 'json'  # 출력 방식 json 또는 xml
+    max_display = 2  # 출력 뉴스 수
+    sort = 'date'  # 결과값의 정렬기준 시간순 date, 관련도 순 sim
+    start = 1  # 출력 위치
+
+    url = f"https://openapi.naver.com/v1/search/news.{encode_type}?query={search}&display={str(int(max_display))}&start={str(int(start))}&sort={sort}"
+
+    # 헤더에 아이디와 키 정보 넣기
+    headers = {'X-Naver-Client-Id': client_id,
+               'X-Naver-Client-Secret': client_secret
+               }
+
+    # HTTP요청 보내기
+    r = requests.get(url, headers=headers)
+    result = []
+    for i in r.json()['items']:
+        tmp_dic = {'title': clean_html(i['title']),
+                   'originallink': i['originallink'],
+                   'link': i['link'],
+                   'description': clean_html(i['description']),
+                   'pubDate': i['pubDate']}
+        result.append(tmp_dic)
+    return render(request, 'test1/index.html', {'result': result})
